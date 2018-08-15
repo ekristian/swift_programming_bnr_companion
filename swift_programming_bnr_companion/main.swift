6c2594d3a468c8c4c7fb130b80be051392e053a4
E. Kristian Heidenreich <ekristian@icloud.com>
2018-08-14 22:01:24 -0400

    Listing 1.1 Proper punctuation

--- swift_programming_bnr_companion/main.swift
+++ swift_programming_bnr_companion/main.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 E. Kristian Heidenreich. All rights reserved.
 //
 
-import Foundation
+import Cocoa
 
-print("Hello, World!")
-
+var str = "Hello, playground"
+str += "!" // += addition and assignment in a single operator
