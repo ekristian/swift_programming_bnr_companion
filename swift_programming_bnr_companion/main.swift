6222c796bfc9849873ae432cc69100db439687ed
E. Kristian Heidenreich <ekristian@icloud.com>
2018-08-15 21:04:02 -0400

    Listing 2.9 Crafting the town description

--- swift_programming_bnr_companion/main.swift
+++ swift_programming_bnr_companion/main.swift
@@ -12,6 +12,5 @@ let numberOfStoplights: Int = 4
 var population: Int
 population = 5422
 let townName: String = "Knowwhere"
-
-print(population)
-print(townName)
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights."
+print(townDescription)
