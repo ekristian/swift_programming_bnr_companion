a0b5e99e80d348ec839d0eb123e8a9a49e3d33e7
E. Kristian Heidenreich <ekristian@icloud.com>
2018-08-16 00:22:48 -0400

    Is there a post office?

--- swift_programming_bnr_companion/main.swift
+++ swift_programming_bnr_companion/main.swift
@@ -10,11 +10,15 @@ import Cocoa
 
 var population: Int = 5422
 var message: String
+var hasPostOffice: Bool = true
 
 if population < 10000 {
     message = "\(population) is a small town!"
 } else {
     message = "\(population) is pretty big!"
 }
-
 print(message)
+
+if !hasPostOffice {
+    print("Where do we buy stamps?")
+}
